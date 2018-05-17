4a3b40979b350ac8843806d8c6c0af600873b0a1
zhangchongjie <778280151@qq.com>
2018-05-17 15:13:04 +0800

    first

---
+++ src/main/java/com/safecode/amber/controller/xxx.java
@@ -0,0 +1,15 @@
+package com.safecode.amber.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.RequestWrapper;
+
+@RestController
+public class xxx {
+    @RequestMapping("")
+    public void kk() {
+
+    }
+}
