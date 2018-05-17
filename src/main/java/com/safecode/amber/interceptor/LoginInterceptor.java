e90bad067849e220de0572870c2c5256ad9c241e
zhangchongjie <778280151@qq.com>
2018-05-17 18:13:05 +0800

    增加拦截器

--- src/main/java/com/safecode/amber/interceptor/LoginInterceptor.java
+++ src/main/java/com/safecode/amber/interceptor/LoginInterceptor.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpSession;
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         System.out.println("Interceptor1 start work");
 //        HttpSession session = request.getSession();
 //        if (session.getAttribute("user") == null) {
@@ -22,13 +22,13 @@ public class LoginInterceptor implements HandlerInterceptor {
         return true;
     }
 
-//    @Override
-//    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-//        System.out.println("Interceptor1 before return view");
-//    }
-//
-//    @Override
-//    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-//        System.out.println("Interceptor1 finish work");
-//    }
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)  {
+        System.out.println("Interceptor1 before return view");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)  {
+        System.out.println("Interceptor1 finish work");
+    }
 }
